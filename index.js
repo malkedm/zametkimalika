966cc9d1c9662d7d7e62003f548ca2f0d539028a
malkedm <136933910+malkedm@users.noreply.github.com>
2023-07-23 17:40:11 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,31 +1,82 @@
-const sidebar = document.querySelector(".sidebar");
-const burgerMenu = document.querySelector(".header");
-const closeIcon = document.querySelector(".close");
-const overlay = document.querySelector(".overlay");
+const myList = document.getElementById("myList");
+let draggingElement = null;
 
-const sidebar2 = document.querySelector(".sidebar2");
-const burgerMenu2 = document.querySelector(".header2");
-const closeIcon2 = document.querySelector(".close2");
-const overlay2 = document.querySelector(".overlay2");
+// Функции для перемещения элементов
+function handleDragStart(event) {
+    draggingElement = event.target;
+    event.dataTransfer.effectAllowed = "move";
+    event.dataTransfer.setData("text/html", event.target.outerHTML);
+    event.target.classList.add("dragging");
+}
 
-const toggleClass = (element, currentClass) => {
-    element.classList.toggle(currentClass);
-};
+function handleDragOver(event) {
+    event.preventDefault();
+    event.dataTransfer.dropEffect = "move";
+}
 
-const toggleSidebarVisible = () => {
-    toggleClass(sidebar, "sidebar-open");
-    toggleClass(overlay, "overlay-open");
-};
+function handleDrop(event) {
+    event.preventDefault();
+    const target = event.target.closest("li");
+    if (target && target !== draggingElement) {
+        if (event.clientY < target.getBoundingClientRect().top + target.clientHeight / 2) {
+            myList.insertBefore(draggingElement, target);
+        } else {
+            myList.insertBefore(draggingElement, target.nextElementSibling);
+        }
+    }
+    draggingElement.classList.remove("dragging");
+    draggingElement = null;
+}
 
-const toggleSidebar2Visible = () => {
-    toggleClass(sidebar2, "sidebar-open2");
-    toggleClass(overlay2, "overlay-open2");
-};
+// Функция для закрепления элемента в начало списка
+function handleDoubleClick(event) {
+    const target = event.target.closest("li");
+    if (target) {
+        myList.prepend(target);
+    }
+}
 
-burgerMenu.addEventListener("click", toggleSidebarVisible);
-closeIcon.addEventListener("click", toggleSidebarVisible);
-overlay.addEventListener("click", toggleSidebarVisible);
+// Добавляем обработчики событий для перемещения элементов и закрепления в начало списка
+const listItems = myList.querySelectorAll("li");
+listItems.forEach(item => {
+    item.addEventListener("dragstart", handleDragStart);
+    item.addEventListener("dragover", handleDragOver);
+    item.addEventListener("drop", handleDrop);
 
-burgerMenu2.addEventListener("click", toggleSidebar2Visible);
-closeIcon2.addEventListener("click", toggleSidebar2Visible);
-overlay2.addEventListener("click", toggleSidebar2Visible);
+    // Добавляем обработчик события "dblclick" для каждого элемента
+    item.addEventListener("dblclick", handleDoubleClick);
+
+    // Добавляем обработчики тач-событий для мобильных устройств
+    item.addEventListener("touchstart", handleTouchStart, false);
+    item.addEventListener("touchmove", handleTouchMove, false);
+    item.addEventListener("touchend", handleTouchEnd, false);
+});
+
+let touchStartY = 0;
+let touchStartItem = null;
+
+function handleTouchStart(event) {
+    touchStartY = event.changedTouches[0].pageY;
+    touchStartItem = event.target.closest("li");
+}
+
+function handleTouchMove(event) {
+    event.preventDefault();
+    const touchY = event.changedTouches[0].pageY;
+    const deltaY = touchY - touchStartY;
+    if (Math.abs(deltaY) > 10) {
+        touchStartItem.classList.add("dragging");
+    }
+}
+
+function handleTouchEnd(event) {
+    touchStartItem.classList.remove("dragging");
+    const touchY = event.changedTouches[0].pageY;
+    const deltaY = touchY - touchStartY;
+    if (Math.abs(deltaY) > 50) {
+        const target = document.elementFromPoint(event.changedTouches[0].clientX, touchY);
+        if (target && target !== touchStartItem) {
+            myList.insertBefore(touchStartItem, target);
+        }
+    }
+}
