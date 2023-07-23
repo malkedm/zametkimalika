f8d852fc7bfe7445adaa0b3e92a9693b015ceb05
malkedm <136933910+malkedm@users.noreply.github.com>
2023-07-23 20:53:44 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,88 +1,31 @@
-const myList = document.getElementById("myList");
-const addButton = document.getElementById("addButton"); // Кнопка для закрепления элемента
+const sidebar = document.querySelector(".sidebar");
+const burgerMenu = document.querySelector(".header");
+const closeIcon = document.querySelector(".close");
+const overlay = document.querySelector(".overlay");
 
-let draggingElement = null;
+const sidebar2 = document.querySelector(".sidebar2");
+const burgerMenu2 = document.querySelector(".header2");
+const closeIcon2 = document.querySelector(".close2");
+const overlay2 = document.querySelector(".overlay2");
 
-// Функции для перемещения элементов
-function handleDragStart(event) {
-    draggingElement = event.target;
-    event.dataTransfer.effectAllowed = "move";
-    event.dataTransfer.setData("text/html", event.target.outerHTML);
-    event.target.classList.add("dragging");
-}
+const toggleClass = (element, currentClass) => {
+    element.classList.toggle(currentClass);
+};
 
-function handleDragOver(event) {
-    event.preventDefault();
-    event.dataTransfer.dropEffect = "move";
-}
+const toggleSidebarVisible = () => {
+    toggleClass(sidebar, "sidebar-open");
+    toggleClass(overlay, "overlay-open");
+};
 
-function handleDrop(event) {
-    event.preventDefault();
-    const target = event.target.closest("li");
-    if (target && target !== draggingElement) {
-        if (event.clientY < target.getBoundingClientRect().top + target.clientHeight / 2) {
-            myList.insertBefore(draggingElement, target);
-        } else {
-            myList.insertBefore(draggingElement, target.nextElementSibling);
-        }
-    }
-    draggingElement.classList.remove("dragging");
-    draggingElement = null;
-}
+const toggleSidebar2Visible = () => {
+    toggleClass(sidebar2, "sidebar-open2");
+    toggleClass(overlay2, "overlay-open2");
+};
 
-// Функция для закрепления элемента в начало списка
-function handleDoubleClick(event) {
-    const target = event.target.closest("li");
-    if (target) {
-        myList.prepend(target);
-    }
-}
+burgerMenu.addEventListener("click", toggleSidebarVisible);
+closeIcon.addEventListener("click", toggleSidebarVisible);
+overlay.addEventListener("click", toggleSidebarVisible);
 
-// Добавляем обработчики событий для перемещения элементов и закрепления в начало списка
-const listItems = myList.querySelectorAll("li");
-listItems.forEach(item => {
-    item.addEventListener("dragstart", handleDragStart);
-    item.addEventListener("dragover", handleDragOver);
-    item.addEventListener("drop", handleDrop);
-    item.addEventListener("dblclick", handleDoubleClick); // Добавляем обработчик события "dblclick" для каждого элемента
-
-    // Добавляем обработчики сенсорных событий для мобильных устройств
-    item.addEventListener("touchstart", handleTouchStart, { passive: false });
-    item.addEventListener("touchmove", handleTouchMove, { passive: false });
-    item.addEventListener("touchend", handleTouchEnd);
-});
-
-let touchStartY = 0;
-let touchStartItem = null;
-
-function handleTouchStart(event) {
-    const target = event.target.closest("li");
-    if (target) {
-        touchStartY = event.touches[0].clientY;
-        touchStartItem = target;
-    }
-}
-
-function handleTouchMove(event) {
-    if (!touchStartItem) return;
-
-    const touchCurrentY = event.touches[0].clientY;
-    const yOffset = touchCurrentY - touchStartY;
-    touchStartY = touchCurrentY;
-
-    const rect = touchStartItem.getBoundingClientRect();
-    const currentIndex = [...myList.children].indexOf(touchStartItem);
-
-    if (yOffset < 0 && currentIndex > 0) {
-        myList.insertBefore(touchStartItem, myList.children[currentIndex - 1]);
-    } else if (yOffset > 0 && currentIndex < myList.children.length - 1) {
-        myList.insertBefore(touchStartItem, myList.children[currentIndex + 2]);
-    }
-}
-
-function handleTouchEnd() {
-    touchStartItem = null;
-}
-
-// Добавляем обработчик события для кнопки закрепления элемента в начале списка
-addButton.addEventListener("click", handleDoubleClick);
+burgerMenu2.addEventListener("click", toggleSidebar2Visible);
+closeIcon2.addEventListener("click", toggleSidebar2Visible);
+overlay2.addEventListener("click", toggleSidebar2Visible);
