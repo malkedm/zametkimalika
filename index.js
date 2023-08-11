636a10b6e581d7e776e48d33a0c088a54540bb49
malkedm <136933910+malkedm@users.noreply.github.com>
2023-08-11 17:31:52 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Получаем кнопку и детали
 const button1 = document.querySelector('.button1');
-const details1 = document.querySelector('#details1');
+const details1 = document.querySelector('.details1');
 
 // Добавляем обработчик события для button1
 button1.addEventListener('click', function() {
