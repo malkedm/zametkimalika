e45039e401b2910f41020db9dbf9ab9b224fdc02
malkedm <136933910+malkedm@users.noreply.github.com>
2023-08-25 12:56:12 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const sidebar = document.querySelector(".sidebar");
 const burgerMenu = document.querySelector(".header");
-const closeIcon = document.querySelector(".close");
+const closeIcon = document.querySelector(".close__item");
 const overlay = document.querySelector(".overlay");
 
 const sidebar2 = document.querySelector(".sidebar2");
 const burgerMenu2 = document.querySelector(".header2");
-const closeIcon2 = document.querySelector(".close2");
+const closeIcon2 = document.querySelector(".close__item2");
 const overlay2 = document.querySelector(".overlay2");
 
 const toggleClass = (element, currentClass) => {
