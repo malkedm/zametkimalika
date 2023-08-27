a98ddb642c1f3f215ba62a4485c0ec7b0fe6e39a
malkedm <136933910+malkedm@users.noreply.github.com>
2023-08-27 15:48:39 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,34 +1,4 @@
-const sidebar = document.querySelector(".sidebar");
-const burgerMenu = document.querySelector(".header");
-const closeIcon = document.querySelector(".close__item");
-const overlay = document.querySelector(".overlay");
 
-const sidebar2 = document.querySelector(".sidebar2");
-const burgerMenu2 = document.querySelector(".header2");
-const closeIcon2 = document.querySelector(".close__item2");
-const overlay2 = document.querySelector(".overlay2");
-
-const toggleClass = (element, currentClass) => {
-    element.classList.toggle(currentClass);
-};
-
-const toggleSidebarVisible = () => {
-    toggleClass(sidebar, "sidebar-open");
-    toggleClass(overlay, "overlay-open");
-};
-
-const toggleSidebar2Visible = () => {
-    toggleClass(sidebar2, "sidebar-open2");
-    toggleClass(overlay2, "overlay-open2");
-};
-
-burgerMenu.addEventListener("click", toggleSidebarVisible);
-closeIcon.addEventListener("click", toggleSidebarVisible);
-overlay.addEventListener("click", toggleSidebarVisible);
-
-burgerMenu2.addEventListener("click", toggleSidebar2Visible);
-closeIcon2.addEventListener("click", toggleSidebar2Visible);
-overlay2.addEventListener("click", toggleSidebar2Visible);
 
 // Создаем массив для хранения закрепленных элементов
 const pinnedElements = [];
