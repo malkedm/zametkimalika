f43da865bdb447d239fd81d6a0a0ce535f6deecd
malkedm <136933910+malkedm@users.noreply.github.com>
2023-10-01 02:17:02 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,109 +1,63 @@
+// Функция для добавления класса "open" к элементам с указанными классами
+function addOpen(classNames) {
+    classNames.forEach(function (className) {
+        const elements = document.querySelectorAll("." + className);
+        elements.forEach(function (element) {
+            element.classList.add("open");
+        });
+    });
+}
 
+function removeOpen(classNames) {
+    classNames.forEach(function (className) {
+        const elements = document.querySelectorAll("." + className);
+        elements.forEach(function (element) {
+            element.classList.remove("open");
+        });
+    });
+}
 
-const sidebar = document.querySelector(".sidebar");
-const burgerMenu = document.querySelector(".header");
-const closeIcon = document.querySelector(".close__item");
-const overlay = document.querySelector(".overlay");
+// Найдите кнопку с нужным классом
+const burger = document.querySelector(".burger");
+burger.addEventListener("click", function () {
+    // Вызовите функцию addOpen с массивом классов, которым нужно добавить класс "open"
+    addOpen(["sidebar", "overlay"]);
+});
 
-const sidebar2 = document.querySelector(".sidebar2");
-const burgerMenu2 = document.querySelector(".header2");
-const closeIcon2 = document.querySelector(".close__item2");
-const overlay2 = document.querySelector(".overlay2");
+const buttonOpenModal = document.querySelector(".btn-start-training");
+buttonOpenModal.addEventListener("click", function () {
+    addOpen(["modal-background", "modal"]);
+});
 
-const toggleClass = (element, currentClass) => {
-    element.classList.toggle(currentClass);
-};
+const buttonClose = document.querySelectorAll(".close__item");
+buttonClose.forEach(function (remOpen) {
+    remOpen.addEventListener("click", function () {
+        removeOpen(["sidebar", "overlay", "modal"]);
+    });
+});
 
-const toggleSidebarVisible = () => {
-    toggleClass(sidebar, "sidebar-open");
-    toggleClass(overlay, "overlay-open");
-};
-
-const toggleSidebar2Visible = () => {
-    toggleClass(sidebar2, "sidebar-open2");
-    toggleClass(overlay2, "overlay-open2");
-};
-
-burgerMenu.addEventListener("click", toggleSidebarVisible);
-closeIcon.addEventListener("click", toggleSidebarVisible);
-overlay.addEventListener("click", toggleSidebarVisible);
-
-document.querySelector(".btn-start-training").addEventListener("click", function () {
-    document.querySelector(".modal").classList.add("open")
-})
-
-document.querySelector(".btn-close-modal").addEventListener("click", function () {
-    document.querySelector(".modal").classList.remove("open")
-})
-
-burgerMenu2.addEventListener("click", toggleSidebar2Visible);
-closeIcon2.addEventListener("click", toggleSidebar2Visible);
-overlay2.addEventListener("click", toggleSidebar2Visible);
-
-
-
-
-
-
-
-
-
-// Создаем массив для хранения закрепленных элементов
-const pinnedElements = [];
-
-// Добавляем обработчик клика на кнопку с emoji &#x1F4CC;
-const buttons = document.querySelectorAll(".header-summary-brp button");
-buttons.forEach(button => {
-    button.addEventListener("click", () => {
-        const parentSummary = button.closest("summary");
-        const parentDetails = button.closest("details");
-        const parentList = parentDetails.parentElement;
-
-        // Проверяем, является ли элемент summary закрепленным
-        const isPinned = pinnedElements.includes(parentDetails);
-
-        if (isPinned) {
-            // Если элемент уже закреплен, то удаляем его из списка закрепленных
-            const index = pinnedElements.indexOf(parentDetails);
-            if (index > -1) {
-                pinnedElements.splice(index, 1);
-            }
-            button.classList.remove("active");
-        } else {
-            // Если элемент не закреплен, то добавляем его в начало списка закрепленных
-            pinnedElements.unshift(parentDetails);
-            button.classList.add("active");
+const backgroundClose = document.querySelectorAll(".background-close");
+backgroundClose.forEach(function (remOpen) {
+    remOpen.addEventListener("click", function (event) {
+        if (event.target.closest(".modal-box")) {
+            return;
         }
-
-        // Перемещаем закрепленные элементы в нужное место в основном списке
-        parentList.prepend(...pinnedElements);
+        removeOpen(["sidebar", "overlay", "modal"]);
     });
 });
 
 
-// Начало кода...
-// Ищем все кнопки button(отвечающие за открытие/закрытие всех элементов details) и сами элементы details
-const openCloseDetailsButtonLeft = document.querySelectorAll("[data-button-open-close-left-panel]"); // Поиск всех кнопок левой панели
-const openCloseDetailsButtonRight = document.querySelectorAll("[data-button-open-close-right-panel]"); // Поиск всех кнопок правой панели
-const openCloseDetailsSpheres = document.querySelectorAll(".details-spheres"); // Поиск всех details левой панели(поиск всех элементов учебных сфер)
-const openCloseDetailsQuestion = document.querySelectorAll(".details-question"); // Поиск всех details правой панели(поиск всех элементов вопрос - ответ)
-const strelkaVnizOpenCloseRightPanel = document.querySelector(".element-open-close-right-panel");
-const strelkaVverhOpenCloseRightPanel = document.querySelector(".element2-open-close-right-panel");
 
+
+
+
+
+
+
+const openCloseDetailsButtonLeft = document.querySelectorAll("[data-button-open-close-left-panel]"); // Поиск всех кнопок левой панели
+const openCloseDetailsSpheres = document.querySelectorAll(".details-spheres"); // Поиск всех details левой панели(поиск всех элементов учебных сфер)
 const strelkaVnizOpenCloseLeftPanel = document.querySelector(".element-open-close-left-panel");
 const strelkaVverhOpenCloseLeftPanel = document.querySelector(".element2-open-close-left-panel");
-
-
-
-// Делаем полноценные функции для левой панели
-/* let isOpenDetailsLeftPanel = false;   !!!!!!!!!!! ВНИМАНИЕ! НЕ СЛЕДУЕТ ИСПОЛЬЗОВАТЬ ГЛОБАЛЬНЫЕ ПЕРЕМЕННЫЕ ВНЕ ФУНКЦИЙ,
-ИНАЧЕ БУДУТ КОНФЛИКТЫ ДВУХ ФУНКЦИЙ(а точнее двух кнопок в функциях.
-Данную тему надо более подробно изучить.
-Но скорее всего, это происходит из за того, что при создании глобальной переменной и указании состояния,
-она применяется сразу ко всему коду, который находится снизу.) */
-
-
-
 openCloseDetailsButtonLeft.forEach(function (left) {
     // Делаем полноценные функции для левой панели
     let isOpen = false; // комментарий выше относится к данной переменной.
@@ -188,66 +142,6 @@ openCloseDetailsButtonLeft.forEach(function (left) {
     summaryList.forEach(img => {
         img.addEventListener("click", event => {
             event.stopPropagation();
-        });
-    });
-});
-
-
-// Делаем полноценные функции для правой панели
-/* let isOpenDetailsRightPanel = false;   !!!!!!!!!!! ВНИМАНИЕ! НЕ СЛЕДУЕТ ИСПОЛЬЗОВАТЬ ГЛОБАЛЬНЫЕ ПЕРЕМЕННЫЕ ВНЕ ФУНКЦИЙ,
-ИНАЧЕ БУДУТ КОНФЛИКТЫ ДВУХ ФУНКЦИЙ(а точнее двух кнопок в функциях.
-Данную тему надо более подробно изучить.
-Но скорее всего, это происходит из за того, что при создании глобальной переменной и указании состояния,
-она применяется сразу ко всему коду, который находится снизу.) */
-
-openCloseDetailsButtonRight.forEach(function (right) {
-    // Делаем полноценные функции для правой панели
-
-    let isOpen = false; // комментарий выше относится к данной переменной.
-    right.addEventListener("click", function () {
-        openCloseDetailsQuestion.forEach(openCloseDetailsQuestion => {
-            if (isOpen) {
-                openCloseDetailsQuestion.removeAttribute("open");
-            } else {
-                openCloseDetailsQuestion.setAttribute("open", true);
-            }
-        });
-
-        isOpen = !isOpen;
-
-        if (isOpen) {
-            strelkaVnizOpenCloseRightPanel.style.display = "none";
-            strelkaVverhOpenCloseRightPanel.style.display = "block";
-        } else {
-            strelkaVverhOpenCloseRightPanel.style.display = "none";
-            strelkaVnizOpenCloseRightPanel.style.display = "block";
-        }
-    });
-
-    // Добавляем функции для закрытия открытых элементов при нажатии на любую область блока и запрещаем, если кнопка открытия/закрытия активна.
-    const blockBottomRightPanel = document.querySelector(".block-bottom-right-panel");
-    blockBottomRightPanel.addEventListener("click", event => {
-        const target = event.target;
-        const isDetailsQuestion = target.closest(".details-question");
-        const isButton = target.classList.contains("butt");
-
-        if (!isOpen && !(isDetailsQuestion || isButton)) {
-            // Если open-close-details неактивна и клик был на другом элементе, разрешаем удаление значения open
-            const detailsList = document.querySelectorAll(".details-question");
-            detailsList.forEach(details => {
-                details.removeAttribute("open");
-            });
-        }
-    });
-
-    // Добавляем обработчик клика на элементы details с классом details-question
-    const detailsList = document.querySelectorAll(".details-question");
-    detailsList.forEach(details => {
-        details.addEventListener("click", event => {
-            // Проверяем, активно ли open-close-details
-            if (isOpen) {
-                event.preventDefault(); // Предотвращаем удаление значения open
-            }
         });
     });
 });
