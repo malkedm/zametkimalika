2faf63c7512ba61aa184f304e62432e8bb184175
malkedm <136933910+malkedm@users.noreply.github.com>
2023-12-06 13:44:50 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,44 +1,31 @@
-document.addEventListener("DOMContentLoaded", function () {
-    // Получаем все ссылки с классом .img-site-1 до .img-site-9
-    const links = document.querySelectorAll('.right-container-header .mini-img');
+document.addEventListener('DOMContentLoaded', function () {
+    const sections = document.querySelectorAll('main section');
+    const links = document.querySelectorAll('header a');
 
-    // Получаем футер
-    const footer = document.querySelector('footer');
-
-    // Функция для изменения стиля ссылок в зависимости от положения скролла
-    function changeLinkStyle() {
-        const scrollPosition = window.scrollY;
-        const footerPosition = footer.offsetTop;
-
-        // Функция для сброса стилей у всех ссылок
-        function resetStyles() {
-            links.forEach(link => {
-                link.style.background = '';
-            });
-        }
-
-        // Перебираем все секции и изменяем стиль ссылок в зависимости от положения скролла
-        document.querySelectorAll('section').forEach((section, index) => {
-            const sectionTop = section.offsetTop;
-            const sectionBottom = sectionTop + section.offsetHeight;
-
-            if (scrollPosition >= sectionTop && scrollPosition < sectionBottom) {
-                // Мы находимся в данной секции, сбрасываем стили и изменяем цвет фона у соответствующей ссылки
-                resetStyles();
-                links[index].style.background = 'green';
-            }
+    function setActiveLink() {
+        const currentSection = Array.from(sections).find(section => {
+            const rect = section.getBoundingClientRect();
+            return rect.top <= 0 && rect.bottom > 0;
         });
 
-        // Проверяем, находимся ли в футере и изменяем цвет фона у соответствующей ссылки
-        if (scrollPosition >= footerPosition) {
-            resetStyles();
-            links[8].style.background = 'green'; // 8 - индекс футера
-        }
+        links.forEach(link => {
+            const sectionId = link.getAttribute('href').substring(1);
+            const correspondingSection = document.getElementById(sectionId);
+
+            if (correspondingSection === currentSection) {
+                link.classList.add('active');
+                link.style.background = 'green';
+            } else {
+                link.classList.remove('active');
+                // Вернуть оригинальный цвет (может потребоваться изменить)
+                link.style.background = '';
+            }
+        });
     }
 
-    // Добавляем обработчик события при прокрутке страницы
-    window.addEventListener('scroll', changeLinkStyle);
-
-    // Вызываем функцию при загрузке страницы для установки начальных стилей
-    changeLinkStyle();
+    // Вызывать функцию при прокрутке страницы
+    document.addEventListener('scroll', setActiveLink);
+    
+    // Вызывать функцию также при загрузке страницы
+    setActiveLink();
 });
