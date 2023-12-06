9891831f8b9baf40ccf8e94bd3fc7ff0bc813e0d
malkedm <136933910+malkedm@users.noreply.github.com>
2023-12-06 13:13:28 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,51 +1,24 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const sections = document.querySelectorAll('section');
-    const footer = document.querySelector('footer');
-    const headerLinks = document.querySelectorAll('header .mini-img');
+    const sections = document.querySelectorAll('section, footer');
+    const headerLinks = document.querySelectorAll('header a');
 
-    let activeIndex = 0;
-
-    function setActiveIndex() {
+    function setActiveLink() {
         const scrollPosition = window.scrollY;
 
         sections.forEach((section, index) => {
-            const sectionTop = section.offsetTop;
+            const sectionTop = section.offsetTop - document.querySelector('header').offsetHeight;
             const sectionBottom = sectionTop + section.offsetHeight;
 
             if (scrollPosition >= sectionTop && scrollPosition < sectionBottom) {
-                activeIndex = index;
-            }
-        });
+                headerLinks.forEach((link) => {
+                    link.classList.remove('a-active');
+                });
 
-        const footerTop = footer.offsetTop;
-        const footerBottom = footerTop + footer.offsetHeight;
-
-        if (scrollPosition >= footerTop && scrollPosition < footerBottom) {
-            activeIndex = headerLinks.length - 1; // Footer is considered as the last section
-        }
-    }
-
-    function changeBackground() {
-        headerLinks.forEach((link, index) => {
-            if (index === activeIndex) {
-                link.classList.add('active');
-            } else {
-                link.classList.remove('active');
+                headerLinks[index].classList.add('a-active');
             }
         });
     }
 
-    function handleScroll() {
-        setActiveIndex();
-        changeBackground();
-    }
-
-    // Listen for scroll events
-    window.addEventListener('scroll', handleScroll);
-
-    // Listen for touch events (for mobile)
-    window.addEventListener('touchmove', handleScroll);
-
-    // Listen for wheel events
-    window.addEventListener('wheel', handleScroll);
+    document.addEventListener('scroll', setActiveLink);
+    window.addEventListener('resize', setActiveLink);
 });
