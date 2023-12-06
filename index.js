0fac9364cf1d24ca427742275c32a0ec9d03da36
malkedm <136933910+malkedm@users.noreply.github.com>
2023-12-06 13:24:52 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,32 +1,27 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const sections = document.querySelectorAll("main section");
-    const footer = document.querySelector("footer");
-    const miniImgs = document.querySelectorAll(".right-container-header .mini-img");
+  // Получаем все ссылки в правом контейнере
+  const links = document.querySelectorAll(".right-container-header .mini-img");
 
-    function setActiveSection() {
-        const scrollPosition = window.scrollY + window.innerHeight / 2;
+  // Обработчик события прокрутки
+  document.addEventListener("scroll", function () {
+    // Получаем текущее положение страницы
+    const currentPosition = window.scrollY;
 
-        sections.forEach((section, index) => {
-            const sectionTop = section.offsetTop;
-            const sectionBottom = sectionTop + section.clientHeight;
+    // Обходим все секции и определяем, в какой секции находится страница
+    document.querySelectorAll("main section").forEach(function (section, index) {
+      const sectionTop = section.offsetTop;
+      const sectionBottom = sectionTop + section.clientHeight;
 
-            if (scrollPosition >= sectionTop && scrollPosition <= sectionBottom) {
-                miniImgs.forEach((img) => img.classList.remove("a-active"));
-                miniImgs[index].classList.add("a-active");
-            }
+      // Проверяем, находится ли текущее положение страницы в пределах текущей секции
+      if (currentPosition >= sectionTop && currentPosition < sectionBottom) {
+        // Удаляем класс .a-active у всех ссылок
+        links.forEach(function (link) {
+          link.classList.remove("a-active");
         });
 
-        // Check if in the footer
-        const footerTop = footer.offsetTop;
-        if (scrollPosition >= footerTop) {
-            miniImgs.forEach((img) => img.classList.remove("a-active"));
-            miniImgs[8].classList.add("a-active"); // Assuming the last mini-img corresponds to the footer
-        }
-    }
-
-    // Add scroll event listener
-    window.addEventListener("scroll", setActiveSection);
-
-    // Trigger the setActiveSection function on page load
-    setActiveSection();
+        // Добавляем класс .a-active к соответствующей ссылке
+        links[index].classList.add("a-active");
+      }
+    });
+  });
 });
