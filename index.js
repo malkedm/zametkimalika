c57a82cdb3e42c41c7af61db5537b70a9d361b8f
malkedm <136933910+malkedm@users.noreply.github.com>
2023-12-06 15:16:58 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,44 +1,35 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const sections = document.querySelectorAll('section');
-    const footer = document.getElementById('footer-9');
+    const sections = document.querySelectorAll('main .main-container section');
+    const footer = document.querySelector('main .main-container footer');
 
-    function getCurrentSection() {
-        const scrollPosition = window.scrollY + window.innerHeight / 2;
-
-        for (const section of sections) {
-            const sectionTop = section.offsetTop;
-            const sectionBottom = sectionTop + section.offsetHeight;
-
-            if (scrollPosition >= sectionTop && scrollPosition < sectionBottom) {
-                return section.id;
-            }
-        }
-
-        if (scrollPosition >= footer.offsetTop && scrollPosition < footer.offsetTop + footer.offsetHeight) {
-            return footer.id;
-        }
-
-        return null;
+    function isInViewport(element) {
+        const rect = element.getBoundingClientRect();
+        return (
+            rect.top >= 0 &&
+            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight)
+        );
     }
 
     function highlightCurrentSection() {
-        const currentSectionId = getCurrentSection();
+        for (const section of sections) {
+            if (isInViewport(section)) {
+                section.classList.add('active');
+            } else {
+                section.classList.remove('active');
+            }
+        }
 
-        // Удалите класс active у всех элементов
-        sections.forEach(section => {
-            section.classList.remove('active');
-        });
-
-        // Добавьте класс active к текущей секции
-        const currentSection = document.getElementById(currentSectionId);
-        if (currentSection) {
-            currentSection.classList.add('active');
+        // Check if footer is in viewport
+        if (isInViewport(footer)) {
+            footer.classList.add('active');
+        } else {
+            footer.classList.remove('active');
         }
     }
 
-    // Вызывайте функцию при прокрутке страницы
-    document.addEventListener('scroll', highlightCurrentSection);
-
-    // Вызывайте функцию еще раз после загрузки страницы для начальной установки
+    // Initial highlighting
     highlightCurrentSection();
+
+    // Update on scroll
+    window.addEventListener('scroll', highlightCurrentSection);
 });
