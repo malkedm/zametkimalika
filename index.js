f302d399025c698ff170978c397bf742658fdf0e
malkedm <136933910+malkedm@users.noreply.github.com>
2023-12-06 12:30:01 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,32 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener('DOMContentLoaded', function () {
     const headerLinks = document.querySelectorAll('header a');
-    const sections = document.querySelectorAll('main .main-container section');
-    const footer = document.querySelector('main .main-container footer');
+    const sections = document.querySelectorAll('main section');
+    const footerLink = document.querySelector('footer a');
 
     function setActiveLink() {
-        const currentScroll = window.scrollY + window.innerHeight / 2;
+        const scrollPosition = window.scrollY + window.innerHeight / 2;
 
         sections.forEach((section, index) => {
             const sectionTop = section.offsetTop;
             const sectionBottom = sectionTop + section.offsetHeight;
 
-            if (currentScroll >= sectionTop && currentScroll < sectionBottom) {
-                headerLinks.forEach(link => link.classList.remove('active'));
+            if (scrollPosition >= sectionTop && scrollPosition < sectionBottom) {
+                headerLinks.forEach((link) => link.classList.remove('active'));
                 headerLinks[index].classList.add('active');
             }
         });
 
-        const footerTop = footer.offsetTop;
-        const footerBottom = footerTop + footer.offsetHeight;
-
-        if (currentScroll >= footerTop && currentScroll < footerBottom) {
-            headerLinks.forEach(link => link.classList.remove('active'));
-            headerLinks[headerLinks.length - 1].classList.add('active');
+        // Check if in the footer
+        const footerTop = document.getElementById('footer-9').offsetTop;
+        if (scrollPosition >= footerTop) {
+            headerLinks.forEach((link) => link.classList.remove('active'));
+            footerLink.classList.add('active');
         }
     }
 
-    document.addEventListener('scroll', setActiveLink);
+    // Set active link on page load
+    setActiveLink();
+
+    // Set active link on scroll
+    window.addEventListener('scroll', setActiveLink);
 });
