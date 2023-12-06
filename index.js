c07ccdc564dd2f2890c6b222bfedfde8caed06c2
malkedm <136933910+malkedm@users.noreply.github.com>
2023-12-06 15:05:52 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,32 +1,42 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // Добавьте обработчик событий для всего документа
-    document.addEventListener("scroll", function () {
-        // Получите текущее положение прокрутки страницы
-        var scrollPosition = window.scrollY;
+    const sections = document.querySelectorAll("main section");
+    const footer = document.getElementById("footer-9");
 
-        // Получите высоту шапки (если она фиксирована)
-        var headerHeight = document.querySelector('header').offsetHeight;
+    function getCurrentSection() {
+        const scrollPosition = window.scrollY + 1;
+        let currentSection = null;
 
-        // Получите высоту каждой секции
-        var section1Height = document.querySelector('.section-1').offsetHeight;
-        // Добавьте остальные секции аналогичным образом
+        sections.forEach((section) => {
+            const sectionTop = section.offsetTop;
+            const sectionBottom = sectionTop + section.offsetHeight;
 
-        // Получите высоту футера
-        var footerHeight = document.querySelector('.footer').offsetHeight;
+            if (scrollPosition >= sectionTop && scrollPosition < sectionBottom) {
+                currentSection = section;
+            }
+        });
 
-        // Определите, в какой секции находится положение прокрутки
-        if (scrollPosition >= headerHeight && scrollPosition < headerHeight + section1Height) {
-            console.log("Находитесь в секции 1");
-        } else if (scrollPosition >= headerHeight + section1Height && scrollPosition < headerHeight + section1Height + section2Height) {
-            console.log("Находитесь в секции 2");
-        } else if (scrollPosition >= headerHeight + section1Height + section2Height && scrollPosition < headerHeight + section1Height + section2Height + section3Height) {
-            console.log("Находитесь в секции 3");
+        if (scrollPosition >= footer.offsetTop) {
+            currentSection = footer;
         }
-        // Добавьте аналогичные условия для остальных секций
 
-        // Определите, находится ли положение прокрутки в футере
-        if (scrollPosition >= headerHeight + section1Height + section2Height + section3Height + section4Height + section5Height + section6Height + section7Height + section8Height) {
-            console.log("Находитесь в футере");
+        return currentSection;
+    }
+
+    function highlightCurrentSection() {
+        const currentSection = getCurrentSection();
+
+        // Убираем подсветку со всех секций
+        sections.forEach((section) => {
+            section.classList.remove("highlight");
+        });
+
+        // Подсвечиваем текущую секцию
+        if (currentSection) {
+            currentSection.classList.add("highlight");
         }
-    });
+    }
+
+    // Вызываем функцию при загрузке страницы и при прокрутке
+    window.addEventListener("scroll", highlightCurrentSection);
+    window.addEventListener("load", highlightCurrentSection);
 });
