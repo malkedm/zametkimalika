f8ce03f88f5f798513af81d72f78a05e2585d63f
malkedm <136933910+malkedm@users.noreply.github.com>
2023-12-06 12:57:55 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,40 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const sections = document.querySelectorAll("main .main-container section");
-    const header = document.querySelector("body header");
-    const footer = document.querySelector("body footer");
+document.addEventListener('DOMContentLoaded', function () {
+    // Получаем все ссылки в header
+    const links = document.querySelectorAll('header a');
 
-    const updateHeaderColor = () => {
-        sections.forEach((section) => {
-            const sectionRect = section.getBoundingClientRect();
-            const sectionTop = sectionRect.top;
-            const sectionBottom = sectionRect.bottom;
+    // Получаем все секции и футер
+    const sections = document.querySelectorAll('main .main-container section');
+    const footer = document.querySelector('main .main-container footer');
 
-            if (sectionTop < window.innerHeight / 2 && sectionBottom > window.innerHeight / 2) {
-                const sectionColor = window.getComputedStyle(section).backgroundColor;
-                header.style.backgroundColor = sectionColor;
-                footer.style.backgroundColor = sectionColor;
+    // Функция для обновления цвета фона ссылок
+    function updateLinkBackground() {
+        // Получаем текущую позицию прокрутки
+        const scrollPosition = window.scrollY || window.pageYOffset;
+
+        // Перебираем все секции и футер
+        sections.forEach((section, index) => {
+            const sectionTop = section.offsetTop;
+            const sectionBottom = sectionTop + section.offsetHeight;
+
+            // Проверяем, находится ли текущая секция на экране
+            if (scrollPosition >= sectionTop && scrollPosition < sectionBottom) {
+                // Если да, то меняем background соответствующей ссылки на зелёный
+                links.forEach(link => link.style.background = '');
+                links[index].style.background = 'green';
             }
         });
-    };
 
-    // Call the function initially
-    updateHeaderColor();
+        // Проверяем, находится ли футер на экране
+        const footerTop = footer.offsetTop;
+        const footerBottom = footerTop + footer.offsetHeight;
 
-    // Attach event listener for scroll to update header color
-    window.addEventListener("scroll", updateHeaderColor);
+        if (scrollPosition >= footerTop && scrollPosition < footerBottom) {
+            // Если да, то меняем background соответствующей ссылки на зелёный
+            links.forEach(link => link.style.background = '');
+            links[links.length - 1].style.background = 'green';
+        }
+    }
+
+    // Обновляем background ссылок при прокрутке
+    window.addEventListener('scroll', updateLinkBackground);
 });
