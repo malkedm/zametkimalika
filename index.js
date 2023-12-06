70b442d8c2fb5e6c4114e76c70d160ff4c12e224
malkedm <136933910+malkedm@users.noreply.github.com>
2023-12-06 13:33:44 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,31 +1,34 @@
-document.addEventListener("DOMContentLoaded", function () {
-    // Получаем все ссылки с классом .img-site-1 - .img-site-9
-    const imgSiteLinks = document.querySelectorAll('.right-container-header .mini-img');
+// Получаем все ссылки с классом .img-site-1 до .img-site-9
+const links = document.querySelectorAll('.img-site-1, .img-site-2, .img-site-3, .img-site-4, .img-site-5, .img-site-6, .img-site-7, .img-site-8, .img-site-9');
 
-    // Функция для определения, находится ли верхняя граница секции в видимой области экрана
-    function isInViewport(element) {
-        const rect = element.getBoundingClientRect();
-        return (
-            rect.top >= 0 &&
-            rect.top <= (window.innerHeight || document.documentElement.clientHeight)
-        );
-    }
+// Функция для проверки, находится ли элемент в видимой области окна
+function isInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
 
-    // Функция для обновления стилей ссылок в зависимости от текущей секции (footer)
-    function updateLinkStyles() {
-        imgSiteLinks.forEach((link, index) => {
-            const section = document.querySelector(`.section-${index + 1}`);
-            if (isInViewport(section)) {
-                link.style.background = 'green';
-            } else {
-                link.style.background = ''; // Сбрасываем стиль, если не в секции
-            }
-        });
-    }
+// Функция для обновления цвета фона ссылок
+function updateLinkBackground() {
+    links.forEach(link => {
+        const sectionId = link.getAttribute('href').substring(1); // Получаем id секции из href
+        const section = document.getElementById(sectionId);
 
-    // Слушаем событие прокрутки страницы и обновляем стили
-    window.addEventListener('scroll', updateLinkStyles);
+        // Проверяем, находится ли секция в видимой области
+        if (isInViewport(section)) {
+            link.style.background = 'green';
+        } else {
+            link.style.background = ''; // Сбрасываем цвет, если секция не видна
+        }
+    });
+}
 
-    // Также вызываем функцию при загрузке страницы
-    updateLinkStyles();
-});
+// Слушаем событие прокрутки окна и обновляем цвет фона ссылок
+window.addEventListener('scroll', updateLinkBackground);
+
+// Вызываем функцию при загрузке страницы для установки начального цвета
+updateLinkBackground();
