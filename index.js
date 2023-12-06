f3ba25ab652407654e0d9220ab4bb7248e28019a
malkedm <136933910+malkedm@users.noreply.github.com>
2023-12-06 15:28:47 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,39 +1,37 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const sections = document.querySelectorAll("main .main-container section");
-    const footer = document.getElementById("footer-9");
+document.addEventListener('DOMContentLoaded', function () {
+    const sections = document.querySelectorAll('section');
+    const footer = document.querySelector('footer');
 
     function highlightCurrentSection() {
-        const scrollPosition = window.scrollY;
+        const currentPosition = window.scrollY + window.innerHeight / 2;
 
-        sections.forEach((section) => {
-            const sectionTop = section.getBoundingClientRect().top + window.scrollY;
-            const sectionBottom = sectionTop + section.clientHeight;
+        sections.forEach((section, index) => {
+            const sectionTop = section.offsetTop;
+            const sectionBottom = sectionTop + section.offsetHeight;
 
-            if (scrollPosition >= sectionTop && scrollPosition < sectionBottom) {
-                // Устанавливаем активный класс для текущей секции
-                section.classList.add("active");
-            } else {
-                // Удаляем активный класс для других секций
-                section.classList.remove("active");
+            if (currentPosition >= sectionTop && currentPosition <= sectionBottom) {
+                removeActiveClass();
+                section.classList.add('active');
             }
         });
 
-        // Проверяем, если находимся в футере
-        const footerTop = footer.getBoundingClientRect().top + window.scrollY;
-        const footerBottom = footerTop + footer.clientHeight;
+        const footerTop = footer.offsetTop;
+        const footerBottom = footerTop + footer.offsetHeight;
 
-        if (scrollPosition >= footerTop && scrollPosition < footerBottom) {
-            // Устанавливаем активный класс для футера
-            footer.classList.add("active");
-        } else {
-            // Удаляем активный класс для футера
-            footer.classList.remove("active");
+        if (currentPosition >= footerTop && currentPosition <= footerBottom) {
+            removeActiveClass();
+            footer.classList.add('active');
         }
     }
 
-    // Вызываем функцию при прокрутке страницы
-    window.addEventListener("scroll", highlightCurrentSection);
+    function removeActiveClass() {
+        sections.forEach((section) => {
+            section.classList.remove('active');
+        });
 
-    // Вызываем функцию при загрузке страницы
-    highlightCurrentSection();
+        footer.classList.remove('active');
+    }
+
+    window.addEventListener('scroll', highlightCurrentSection);
+    window.addEventListener('resize', highlightCurrentSection);
 });
