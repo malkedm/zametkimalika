7021b86a594fb463921678473740d93e0743dc8f
malkedm <136933910+malkedm@users.noreply.github.com>
2023-12-06 12:05:21 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,105 +1,70 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const sections = document.querySelectorAll("main .main-container section");
-    const headerLinks = document.querySelectorAll("header a");
-    const header = document.querySelector("body header");
-    const footer = document.querySelector("footer");
+document.addEventListener('DOMContentLoaded', function () {
+  // Получаем все ссылки в контейнере header
+  const headerLinks = document.querySelectorAll('header a');
 
-    let currentSection = 1;
+  // Получаем все секции
+  const sections = document.querySelectorAll('main .main-container section');
 
-    // Функция обновления активных ссылок и цвета header
-    function updateActiveLinksAndHeaderColor() {
-        headerLinks.forEach((link, index) => {
-            if (index + 1 === currentSection) {
-                link.classList.add("active");
-            } else {
-                link.classList.remove("active");
-            }
-        });
-
-        header.style.backgroundColor = getComputedStyle(sections[currentSection - 1]).backgroundColor;
+  // Функция для определения видимой секции
+  function getVisibleSection() {
+    for (const section of sections) {
+      const rect = section.getBoundingClientRect();
+      if (rect.top <= window.innerHeight / 2 && rect.bottom >= window.innerHeight / 2) {
+        return section;
+      }
     }
+    return null;
+  }
 
-    // Функция для бесконечной прокрутки вверх/вниз
-    function scrollToSection(targetSection) {
-        if (targetSection < 1) {
-            currentSection = sections.length;
-        } else if (targetSection > sections.length) {
-            currentSection = 1;
-        } else {
-            currentSection = targetSection;
-        }
+  // Функция для обновления активной ссылки в header
+  function updateActiveLink() {
+    const visibleSection = getVisibleSection();
+    headerLinks.forEach(link => link.classList.remove('active'));
 
-        sections[currentSection - 1].scrollIntoView({
-            behavior: "smooth",
-            block: "start"
-        });
+    if (visibleSection) {
+      const correspondingLink = document.querySelector(`header a[href="#${visibleSection.id}"]`);
+      if (correspondingLink) {
+        correspondingLink.classList.add('active');
 
-        updateActiveLinksAndHeaderColor();
+        // Меняем цвет header в соответствии с цветом активной секции
+        const sectionColor = window.getComputedStyle(visibleSection).backgroundColor;
+        document.querySelector('header').style.backgroundColor = sectionColor;
+      }
     }
+  }
 
-    // Обработчик прокрутки колесиком/свайпом/скроллбаром
-    document.addEventListener("wheel", function (event) {
-        if (event.deltaY > 0) {
-            scrollToSection(currentSection + 1);
-        } else {
-            scrollToSection(currentSection - 1);
-        }
-    });
+  // Обработчик события прокрутки
+  window.addEventListener('scroll', updateActiveLink);
 
-    // Обработчик нажатия на ссылки в header
-    headerLinks.forEach((link, index) => {
-        link.addEventListener("click", function (event) {
-            event.preventDefault();
-            scrollToSection(index + 1);
-        });
-    });
+  // Обработчик события свайпа колесика мыши
+  window.addEventListener('wheel', function (event) {
+    if (event.deltaY > 0) {
+      // Свайп вниз
+      window.scrollBy(0, window.innerHeight);
+    } else {
+      // Свайп вверх
+      window.scrollBy(0, -window.innerHeight);
+    }
+    updateActiveLink();
+  });
 
-    // Обработчик прокрутки при помощи скроллбара
-    document.addEventListener("scroll", function () {
-        const scrollPosition = window.scrollY;
-        const windowHeight = window.innerHeight;
+  // Обработчик события свайпа на сенсорных устройствах
+  let touchStartY;
+  window.addEventListener('touchstart', function (event) {
+    touchStartY = event.touches[0].clientY;
+  });
 
-        for (let i = 0; i < sections.length; i++) {
-            const section = sections[i];
-            const sectionTop = section.offsetTop - $height-header;
-            const sectionBottom = sectionTop + section.clientHeight;
+  window.addEventListener('touchend', function (event) {
+    const touchEndY = event.changedTouches[0].clientY;
+    const deltaY = touchEndY - touchStartY;
 
-            if (scrollPosition >= sectionTop && scrollPosition < sectionBottom) {
-                currentSection = i + 1;
-                updateActiveLinksAndHeaderColor();
-                break;
-            }
-        }
-    });
-
-    // Обработчик свайпа
-    let startY;
-    let endY;
-
-    document.addEventListener("touchstart", function (event) {
-        startY = event.touches[0].clientY;
-    });
-
-    document.addEventListener("touchend", function (event) {
-        endY = event.changedTouches[0].clientY;
-        const deltaY = endY - startY;
-
-        if (deltaY > 50) {
-            scrollToSection(currentSection - 1);
-        } else if (deltaY < -50) {
-            scrollToSection(currentSection + 1);
-        }
-    });
-
-    // Обработчик для бесконечной прокрутки при достижении конца или начала
-    document.addEventListener("wheel", function (event) {
-        const isScrollingDown = event.deltaY > 0;
-        const isScrollingUp = event.deltaY < 0;
-
-        if (isScrollingDown && currentSection === sections.length) {
-            scrollToSection(1);
-        } else if (isScrollingUp && currentSection === 1) {
-            scrollToSection(sections.length);
-        }
-    });
+    if (deltaY > 0) {
+      // Свайп вниз
+      window.scrollBy(0, window.innerHeight);
+    } else {
+      // Свайп вверх
+      window.scrollBy(0, -window.innerHeight);
+    }
+    updateActiveLink();
+  });
 });
