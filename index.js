09faf89468a6334c5a22fb3c46efb41cd97290b2
malkedm <136933910+malkedm@users.noreply.github.com>
2023-12-06 13:22:00 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,47 +1,32 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const sections = document.querySelectorAll('main .main-container section');
-    const footer = document.querySelector('main .main-container footer');
-    const miniImgs = document.querySelectorAll('.right-container-header .mini-img');
+document.addEventListener("DOMContentLoaded", function () {
+    const sections = document.querySelectorAll("main section");
+    const footer = document.querySelector("footer");
+    const miniImgs = document.querySelectorAll(".right-container-header .mini-img");
 
-    window.addEventListener('scroll', function () {
-        let currentSection = null;
+    function setActiveSection() {
+        const scrollPosition = window.scrollY + window.innerHeight / 2;
 
         sections.forEach((section, index) => {
-            const rect = section.getBoundingClientRect();
+            const sectionTop = section.offsetTop;
+            const sectionBottom = sectionTop + section.clientHeight;
 
-            if (rect.top <= 0 && rect.bottom > 0) {
-                currentSection = index + 1;
+            if (scrollPosition >= sectionTop && scrollPosition <= sectionBottom) {
+                miniImgs.forEach((img) => img.classList.remove("a-active"));
+                miniImgs[index].classList.add("a-active");
             }
         });
 
-        if (currentSection !== null) {
-            miniImgs.forEach((img, index) => {
-                img.classList.remove('a-active');
-            });
-
-            const activeImg = document.querySelector(`.right-container-header .img-site-${currentSection}`);
-            if (activeImg) {
-                activeImg.classList.add('a-active');
-            }
-        } else {
-            miniImgs.forEach((img) => {
-                img.classList.remove('a-active');
-            });
+        // Check if in the footer
+        const footerTop = footer.offsetTop;
+        if (scrollPosition >= footerTop) {
+            miniImgs.forEach((img) => img.classList.remove("a-active"));
+            miniImgs[8].classList.add("a-active"); // Assuming the last mini-img corresponds to the footer
         }
-    });
+    }
 
-    window.addEventListener('scroll', function () {
-        const rect = footer.getBoundingClientRect();
+    // Add scroll event listener
+    window.addEventListener("scroll", setActiveSection);
 
-        if (rect.top <= window.innerHeight && rect.bottom > 0) {
-            miniImgs.forEach((img) => {
-                img.classList.remove('a-active');
-            });
-
-            const activeImg = document.querySelector('.right-container-header .img-site-9');
-            if (activeImg) {
-                activeImg.classList.add('a-active');
-            }
-        }
-    });
+    // Trigger the setActiveSection function on page load
+    setActiveSection();
 });
