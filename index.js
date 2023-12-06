039190e5a268e82b3651fd9c2c0c856b1fa538f9
malkedm <136933910+malkedm@users.noreply.github.com>
2023-12-06 13:29:27 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,33 @@
 document.addEventListener("DOMContentLoaded", function () {
-  // Получаем все ссылки в правом контейнере
-  const links = document.querySelectorAll(".right-container-header .mini-img");
+    // Получаем все ссылки с классом .img-site-1 - .img-site-9
+    const imgSiteLinks = document.querySelectorAll('.right-container-header .mini-img');
 
-  // Обработчик события прокрутки
-  document.addEventListener("scroll", function () {
-    // Получаем текущее положение страницы
-    const currentPosition = window.scrollY;
+    // Функция для определения, находится ли элемент в видимой области экрана
+    function isInViewport(element) {
+        const rect = element.getBoundingClientRect();
+        return (
+            rect.top >= 0 &&
+            rect.left >= 0 &&
+            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+            rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+        );
+    }
 
-    // Обходим все секции и определяем, в какой секции находится страница
-    document.querySelectorAll("main section").forEach(function (section, index) {
-      const sectionTop = section.offsetTop;
-      const sectionBottom = sectionTop + section.clientHeight;
-
-      // Проверяем, находится ли текущее положение страницы в пределах текущей секции
-      if (currentPosition >= sectionTop && currentPosition < sectionBottom) {
-        // Удаляем класс .a-active у всех ссылок
-        links.forEach(function (link) {
-          link.classList.remove("a-active");
+    // Функция для обновления стилей ссылок в зависимости от текущей секции (footer)
+    function updateLinkStyles() {
+        imgSiteLinks.forEach((link, index) => {
+            const section = document.querySelector(`.section-${index + 1}`);
+            if (isInViewport(section)) {
+                link.style.background = 'green';
+            } else {
+                link.style.background = ''; // Сбрасываем стиль, если не в секции
+            }
         });
+    }
 
-        // Добавляем класс .a-active к соответствующей ссылке
-        links[index].classList.add("a-active");
-      }
-    });
-  });
+    // Слушаем событие прокрутки страницы и обновляем стили
+    window.addEventListener('scroll', updateLinkStyles);
+
+    // Также вызываем функцию при загрузке страницы
+    updateLinkStyles();
 });
