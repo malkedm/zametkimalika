cdbec4d9bf3f4c5c4ec314ba82ca34a9253702f2
malkedm <136933910+malkedm@users.noreply.github.com>
2023-12-07 10:40:47 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,29 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const header = document.querySelector('header');
-    const sections = document.querySelectorAll('main .main-container section');
+    const sections = document.querySelectorAll('main section');
     const footer = document.querySelector('footer');
+    const header = document.querySelector('header');
 
-    function setActiveSection() {
-        const scrollPosition = window.scrollY + header.offsetHeight;
-
-        sections.forEach((section, index) => {
-            const sectionTop = section.offsetTop;
-            const sectionBottom = sectionTop + section.offsetHeight;
-
-            if (scrollPosition >= sectionTop && scrollPosition < sectionBottom) {
+    // Функция для обновления фона header
+    function updateHeaderBackground() {
+        sections.forEach((section) => {
+            const rect = section.getBoundingClientRect();
+            if (rect.top >= 0 && rect.bottom <= window.innerHeight) {
                 header.style.background = section.style.background;
             }
         });
 
-        const footerTop = footer.offsetTop;
-        const footerBottom = footerTop + footer.offsetHeight;
-
-        if (scrollPosition >= footerTop && scrollPosition < footerBottom) {
+        // Проверка для футера
+        const rectFooter = footer.getBoundingClientRect();
+        if (rectFooter.top >= 0 && rectFooter.bottom <= window.innerHeight) {
             header.style.background = footer.style.background;
         }
     }
 
-    window.addEventListener('scroll', setActiveSection);
-    setActiveSection(); // Вызываем функцию один раз при загрузке страницы
+    // Вызываем функцию при загрузке страницы
+    updateHeaderBackground();
+
+    // Обновляем фон header при скролле
+    document.addEventListener('scroll', function () {
+        updateHeaderBackground();
+    });
 });
