76ea66064cace5dc7cdc4444b993307442f7113f
malkedm <136933910+malkedm@users.noreply.github.com>
2023-12-07 15:08:59 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,43 +3,41 @@ document.addEventListener('DOMContentLoaded', function () {
     const header = document.querySelector('body header');
     const links = document.querySelectorAll('body header .right-container-header a');
 
-    let isScrolling;
+    function isElementInViewport(el) {
+        const rect = el.getBoundingClientRect();
+        return (
+            rect.top <= window.innerHeight / 2 &&
+            rect.bottom >= window.innerHeight / 2
+        );
+    }
 
-    function handleIntersection() {
-        const scrollPosition = window.scrollY;
+    function handleScroll() {
+        let activeSection = null;
 
         sections.forEach((section) => {
-            const rect = section.getBoundingClientRect();
-            if (rect.top <= window.innerHeight / 2 && rect.bottom >= window.innerHeight / 2) {
-                const activeSection = section.getAttribute('id');
-                console.log(`Активная секция: ${activeSection}`);
-                const backgroundColor = window.getComputedStyle(section).backgroundColor;
-                header.style.backgroundColor = backgroundColor;
-
-                links.forEach((link) => {
-                    const linkSection = link.getAttribute('href').substring(1);
-                    if (linkSection === activeSection) {
-                        link.style.backgroundColor = 'green';
-                    } else {
-                        link.style.backgroundColor = ''; // или установите исходный цвет
-                    }
-                });
+            if (isElementInViewport(section)) {
+                activeSection = section.id;
             }
         });
+
+        if (activeSection) {
+            console.log(`Активная секция: ${activeSection}`);
+            const backgroundColor = window.getComputedStyle(document.getElementById(activeSection)).backgroundColor;
+            header.style.backgroundColor = backgroundColor;
+
+            links.forEach((link) => {
+                const linkSection = link.getAttribute('href').substring(1);
+                if (linkSection === activeSection) {
+                    link.style.backgroundColor = 'green';
+                } else {
+                    link.style.backgroundColor = ''; // или установите исходный цвет
+                }
+            });
+        }
     }
 
-    function scrollHandler() {
-        window.clearTimeout(isScrolling);
-        isScrolling = setTimeout(function () {
-            handleIntersection();
-        }, 200);
-    }
+    document.addEventListener('scroll', handleScroll);
 
-    document.addEventListener('scroll', function () {
-        window.requestAnimationFrame(scrollHandler);
-    });
-
-    window.addEventListener('resize', function () {
-        handleIntersection();
-    });
+    // Запускаем handleScroll() при загрузке страницы, чтобы установить начальное состояние
+    handleScroll();
 });
