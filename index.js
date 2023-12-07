d23dc65ffeef7b82775eec1ffa5bbb7f87cdf3ef
malkedm <136933910+malkedm@users.noreply.github.com>
2023-12-07 12:34:29 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,31 +5,23 @@ document.addEventListener('DOMContentLoaded', function () {
 
     function changeHeaderColor() {
         const currentScroll = window.scrollY;
-        let activeSectionIndex = -1;
 
         sections.forEach((section, index) => {
             const sectionTop = section.offsetTop - header.offsetHeight;
             const sectionBottom = sectionTop + section.offsetHeight;
 
             if (currentScroll >= sectionTop && currentScroll < sectionBottom) {
-                activeSectionIndex = index;
+                header.style.backgroundColor = getComputedStyle(section).backgroundColor;
             }
         });
 
         if (currentScroll >= footer.offsetTop - header.offsetHeight) {
-            activeSectionIndex = sections.length; // Footer is considered as the last section
-        }
-
-        if (activeSectionIndex !== -1) {
-            const activeColor = getComputedStyle(sections[activeSectionIndex]).backgroundColor;
-            header.style.background = activeColor;
+            header.style.backgroundColor = getComputedStyle(footer).backgroundColor;
         }
     }
 
-    // Listen for scroll events to trigger color change
     window.addEventListener('scroll', changeHeaderColor);
 
-    // Smooth scrolling when clicking on header links
     document.querySelectorAll('header a').forEach(anchor => {
         anchor.addEventListener('click', function (e) {
             e.preventDefault();
