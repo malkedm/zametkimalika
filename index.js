f4ffc379145348cac08cd99332b4c1da89b14e68
malkedm <136933910+malkedm@users.noreply.github.com>
2023-12-07 12:53:11 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,33 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const sections = document.querySelectorAll("main .main-container section");
-    const header = document.querySelector("body header");
+document.addEventListener('DOMContentLoaded', function () {
+    // Получаем все секции
+    const sections = document.querySelectorAll('main .main-container section');
 
-    function setActiveSection() {
-        let currentScroll = window.scrollY + header.clientHeight;
-        let activeSection = Array.from(sections).findIndex((section) => {
-            const sectionTop = section.offsetTop;
-            const sectionBottom = sectionTop + section.clientHeight;
-            return currentScroll >= sectionTop && currentScroll < sectionBottom;
+    // Функция для определения активной секции
+    function determineActiveSection() {
+        let activeSection = null;
+
+        // Перебираем все секции и проверяем, какая из них находится в видимой области
+        sections.forEach((section, index) => {
+            const rect = section.getBoundingClientRect();
+            if (rect.top >= 0 && rect.bottom <= window.innerHeight) {
+                activeSection = index + 1;
+            }
         });
 
-        if (activeSection !== -1) {
-            updateHeaderColor(activeSection + 1);
+        return activeSection;
+    }
+
+    // Функция для обновления активной секции и вывода в консоль
+    function updateActiveSection() {
+        const activeSection = determineActiveSection();
+        if (activeSection !== null) {
+            console.log(`Активная секция: section-${activeSection}`);
         }
     }
 
-    function updateHeaderColor(sectionNumber) {
-        header.style.background = getComputedStyle(document.querySelector(`.section-${sectionNumber}`)).backgroundColor;
-    }
+    // Добавляем событие прокрутки для отслеживания изменений
+    document.addEventListener('scroll', updateActiveSection);
 
-    window.addEventListener("scroll", setActiveSection);
-    setActiveSection(); // Set initial active section color
+    // Вызываем функцию для первоначальной установки активной секции
+    updateActiveSection();
 });
