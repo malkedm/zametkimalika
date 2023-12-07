d1a072b23a620b16eb1258732d4578e385577828
malkedm <136933910+malkedm@users.noreply.github.com>
2023-12-07 12:22:46 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const sections = document.querySelectorAll("main .main-container section");
+  const footer = document.querySelector("#footer-9");
+  const header = document.querySelector("body header");
 
+  function setActiveSection() {
+    let activeSection = null;
+
+    sections.forEach((section) => {
+      const rect = section.getBoundingClientRect();
+      if (rect.top <= window.innerHeight / 2 && rect.bottom >= window.innerHeight / 2) {
+        activeSection = section;
+      }
+    });
+
+    if (footer.getBoundingClientRect().top <= window.innerHeight / 2) {
+      activeSection = footer;
+    }
+
+    if (activeSection) {
+      const backgroundColor = window.getComputedStyle(activeSection).backgroundColor;
+      header.style.background = backgroundColor;
+    }
+  }
+
+  // Вызываем setActiveSection при загрузке страницы и при скролле
+  window.addEventListener("scroll", setActiveSection);
+  window.addEventListener("load", setActiveSection);
+});
