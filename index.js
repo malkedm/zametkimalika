2aa09f3c609f65a1138ec77937daa490d10ce916
malkedm <136933910+malkedm@users.noreply.github.com>
2023-12-07 15:02:16 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,24 +4,25 @@ document.addEventListener('DOMContentLoaded', function () {
     const links = document.querySelectorAll('body header .right-container-header a');
 
     function handleIntersection(entries, observer) {
-        entries.forEach((entry) => {
-            if (entry.isIntersecting && entry.intersectionRatio === 1.0) {
-                const activeSection = entry.target.getAttribute('id');
-                console.log(`Активная секция: ${activeSection}`);
-                const backgroundColor = window.getComputedStyle(entry.target).backgroundColor;
-                header.style.backgroundColor = backgroundColor;
+        const visibleSections = entries
+            .filter((entry) => entry.isIntersecting && entry.intersectionRatio === 1.0)
+            .map((entry) => entry.target.getAttribute('id'));
 
-                // Меняем цвет активной ссылки на зеленый, а у других возвращаем исходный цвет
-                links.forEach((link) => {
-                    const linkSection = link.getAttribute('href').substring(1);
-                    if (linkSection === activeSection) {
-                        link.style.backgroundColor = 'green';
-                    } else {
-                        link.style.backgroundColor = ''; // или установите исходный цвет
-                    }
-                });
-            }
-        });
+        if (visibleSections.length > 0) {
+            const activeSection = visibleSections[0];
+            console.log(`Активная секция: ${activeSection}`);
+            const backgroundColor = window.getComputedStyle(document.getElementById(activeSection)).backgroundColor;
+            header.style.backgroundColor = backgroundColor;
+
+            links.forEach((link) => {
+                const linkSection = link.getAttribute('href').substring(1);
+                if (linkSection === activeSection) {
+                    link.style.backgroundColor = 'green';
+                } else {
+                    link.style.backgroundColor = ''; // или установите исходный цвет
+                }
+            });
+        }
     }
 
     const observer = new IntersectionObserver(handleIntersection, {
@@ -32,5 +33,10 @@ document.addEventListener('DOMContentLoaded', function () {
 
     sections.forEach((section) => {
         observer.observe(section);
+    });
+
+    // Добавим событие прокрутки для обработки изменений на мобильных устройствах
+    document.addEventListener('scroll', () => {
+        handleIntersection(sections.map((section) => ({ target: section })), observer);
     });
 });
