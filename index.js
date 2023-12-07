93ced827bec60b039ef5318f3704eb36a4258972
malkedm <136933910+malkedm@users.noreply.github.com>
2023-12-07 13:15:04 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
     const sections = document.querySelectorAll('main .main-container section');
     const header = document.querySelector('body header');
+    const links = document.querySelectorAll('body header .right-container-header a');
 
     function handleIntersection(entries, observer) {
         entries.forEach((entry) => {
@@ -10,16 +11,15 @@ document.addEventListener('DOMContentLoaded', function () {
                 const backgroundColor = window.getComputedStyle(entry.target).backgroundColor;
                 header.style.backgroundColor = backgroundColor;
 
-                // Убираем активный класс у всех ссылок
-                document.querySelectorAll('body header .mini-img').forEach(link => {
-                    link.classList.remove('active');
+                // Меняем цвет активной ссылки на зеленый, а у других возвращаем исходный цвет
+                links.forEach((link) => {
+                    const linkSection = link.getAttribute('href').substring(1);
+                    if (linkSection === activeSection) {
+                        link.style.backgroundColor = 'green';
+                    } else {
+                        link.style.backgroundColor = ''; // или установите исходный цвет
+                    }
                 });
-
-                // Добавляем активный класс только активной ссылке
-                const activeLink = document.querySelector(`body header .img-site-${activeSection}`);
-                if (activeLink) {
-                    activeLink.classList.add('active');
-                }
             }
         });
     }
