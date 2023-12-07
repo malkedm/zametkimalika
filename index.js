1a05e2c572cc6d5df5d69440bfabc5fc9fb9ba67
malkedm <136933910+malkedm@users.noreply.github.com>
2023-12-07 13:11:04 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,11 +4,22 @@ document.addEventListener('DOMContentLoaded', function () {
 
     function handleIntersection(entries, observer) {
         entries.forEach((entry) => {
-            if (entry.isIntersecting) {
+            if (entry.isIntersecting && entry.intersectionRatio === 1.0) {
                 const activeSection = entry.target.getAttribute('id');
                 console.log(`Активная секция: ${activeSection}`);
                 const backgroundColor = window.getComputedStyle(entry.target).backgroundColor;
                 header.style.backgroundColor = backgroundColor;
+
+                // Убираем активный класс у всех ссылок
+                document.querySelectorAll('body header .mini-img').forEach(link => {
+                    link.classList.remove('active');
+                });
+
+                // Добавляем активный класс только активной ссылке
+                const activeLink = document.querySelector(`body header .img-site-${activeSection}`);
+                if (activeLink) {
+                    activeLink.classList.add('active');
+                }
             }
         });
     }
@@ -16,7 +27,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const observer = new IntersectionObserver(handleIntersection, {
         root: null,
         rootMargin: '0px',
-        threshold: 0.5,
+        threshold: 1.0,
     });
 
     sections.forEach((section) => {
