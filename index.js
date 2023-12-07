2011831a59ade7166d93f273d9435008dae3e96d
malkedm <136933910+malkedm@users.noreply.github.com>
2023-12-07 13:25:29 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,20 @@
 document.addEventListener('DOMContentLoaded', function () {
     const sections = document.querySelectorAll('main .main-container section');
-    const header = document.querySelector('body header');
-    const links = document.querySelectorAll('body header .right-container-header a');
 
     function handleIntersection(entries, observer) {
         entries.forEach((entry) => {
             if (entry.isIntersecting && entry.intersectionRatio === 1.0) {
                 const activeSection = entry.target.getAttribute('id');
                 console.log(`Активная секция: ${activeSection}`);
-                const backgroundColor = window.getComputedStyle(entry.target).backgroundColor;
-                header.style.backgroundColor = backgroundColor;
-
-                // Меняем цвет активной ссылки на зеленый, а у других возвращаем исходный цвет
-                links.forEach((link) => {
-                    const linkSection = link.getAttribute('href').substring(1);
-                    if (linkSection === activeSection) {
-                        link.style.backgroundColor = 'green';
-                    } else {
-                        link.style.backgroundColor = ''; // или установите исходный цвет
-                    }
-                });
             }
         });
+    }
+
+    function moveToFirstSection() {
+        const firstSection = document.querySelector('#section-1');
+        if (firstSection) {
+            firstSection.scrollIntoView({ behavior: 'smooth' });
+        }
     }
 
     const observer = new IntersectionObserver(handleIntersection, {
@@ -32,5 +25,15 @@ document.addEventListener('DOMContentLoaded', function () {
 
     sections.forEach((section) => {
         observer.observe(section);
+    });
+
+    // Добавим обработчик для бесконечного скролла
+    window.addEventListener('scroll', function () {
+        const lastSection = sections[sections.length - 1];
+        const lastSectionRect = lastSection.getBoundingClientRect();
+        // Если последняя секция видима и прокрутка идет вниз, переместим в первую секцию
+        if (lastSectionRect.top <= window.innerHeight && lastSectionRect.bottom >= window.innerHeight) {
+            moveToFirstSection();
+        }
     });
 });
