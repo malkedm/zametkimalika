800e9cd30290ec4ac01bd788f636f0ed8eabd782
malkedm <136933910+malkedm@users.noreply.github.com>
2023-12-07 10:37:50 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,36 +1,27 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener("DOMContentLoaded", function () {
     const header = document.querySelector('header');
-    const sections = document.querySelectorAll('section');
+    const sections = document.querySelectorAll('main .main-container section');
     const footer = document.querySelector('footer');
 
-    // Функция для обновления фона в шапке
-    function updateHeaderBackground() {
-        const currentSectionId = getCurrentSectionId();
-        const currentFooterBackground = getComputedStyle(footer).backgroundColor;
-
-        header.style.background = currentSectionId ? getSectionBackground(currentSectionId) : currentFooterBackground;
-    }
-
-    // Функция для получения id активной секции
-    function getCurrentSectionId() {
+    // Функция для определения активной секции
+    function getActiveSection() {
         for (const section of sections) {
             const rect = section.getBoundingClientRect();
             if (rect.top >= 0 && rect.bottom <= window.innerHeight) {
-                return section.id;
+                return section;
             }
         }
-        return null;
+        return footer; // Если нет активной секции, то считаем активным footer
     }
 
-    // Функция для получения фона активной секции
-    function getSectionBackground(sectionId) {
-        const sectionElement = document.getElementById(sectionId);
-        return getComputedStyle(sectionElement).backgroundColor;
+    // Функция для установки цвета фона для header
+    function setHeaderBackground() {
+        const activeSection = getActiveSection();
+        const backgroundColor = window.getComputedStyle(activeSection).backgroundColor;
+        header.style.background = backgroundColor;
     }
 
-    // Обновляем фон при прокрутке страницы
-    window.addEventListener('scroll', updateHeaderBackground);
-
-    // Обновляем фон при загрузке страницы
-    window.addEventListener('load', updateHeaderBackground);
+    // Вызываем функцию при загрузке страницы и при прокрутке
+    setHeaderBackground();
+    window.addEventListener('scroll', setHeaderBackground);
 });
