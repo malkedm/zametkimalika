51eee97039f3b96b592f719db180244f936af188
malkedm <136933910+malkedm@users.noreply.github.com>
2023-12-15 13:51:26 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,56 +1,147 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const sections = document.querySelectorAll('main .main-container section');
-    const header = document.querySelector('body header');
-    const links = document.querySelectorAll('body header .right-container-header a');
+// Функция для добавления класса "open" к элементам с указанными классами
+function addOpen(classNames) {
+    classNames.forEach(function (className) {
+        const elements = document.querySelectorAll("." + className);
+        elements.forEach(function (element) {
+            element.classList.add("open");
+        });
+    });
+}
 
-    function handleIntersection(entries, observer) {
-        const visibleSections = entries
-            .filter((entry) => entry.isIntersecting && entry.intersectionRatio > 0.7)
-            .map((entry) => entry.target.getAttribute('id'));
+function removeOpen(classNames) {
+    classNames.forEach(function (className) {
+        const elements = document.querySelectorAll("." + className);
+        elements.forEach(function (element) {
+            element.classList.remove("open");
+        });
+    });
+}
 
-        if (visibleSections.length > 0) {
-            const activeSection = visibleSections[0];
-            console.log(`Активная секция: ${activeSection}`);
-            const backgroundColor = window.getComputedStyle(document.getElementById(activeSection)).backgroundColor;
-            header.style.backgroundColor = backgroundColor;
+// Найдите кнопку с нужным классом
+const burger = document.querySelector(".burger");
+burger.addEventListener("click", function () {
+    // Вызовите функцию addOpen с массивом классов, которым нужно добавить класс "open"
+    addOpen(["sidebar", "overlay"]);
+});
 
-            links.forEach((link) => {
-                const linkSection = link.getAttribute('href').substring(1);
-                if (linkSection === activeSection) {
-                    link.style.backgroundColor = '#b7ffe9';
-                } else {
-                    link.style.backgroundColor = ''; // или установите исходный цвет
-                }
-            });
+const buttonOpenModal = document.querySelector(".btn-start-training");
+buttonOpenModal.addEventListener("click", function () {
+    addOpen(["modal-background", "modal"]);
+});
+
+const buttonClose = document.querySelectorAll(".close__item");
+buttonClose.forEach(function (remOpen) {
+    remOpen.addEventListener("click", function () {
+        removeOpen(["sidebar", "overlay", "modal"]);
+    });
+});
+
+const backgroundClose = document.querySelectorAll(".background-close");
+backgroundClose.forEach(function (remOpen) {
+    remOpen.addEventListener("click", function (event) {
+        if (event.target.closest(".modal-box")) {
+            return;
         }
-    }
+        removeOpen(["sidebar", "overlay", "modal"]);
+    });
+});
 
-    const observer = new IntersectionObserver(handleIntersection, {
-        root: null,
-        rootMargin: '0px',
-        threshold: 0.90,
+
+
+
+
+
+
+
+
+const openCloseDetailsButtonLeft = document.querySelectorAll("[data-button-open-close-left-panel]"); // Поиск всех кнопок левой панели
+const openCloseDetailsSpheres = document.querySelectorAll(".details-spheres"); // Поиск всех details левой панели(поиск всех элементов учебных сфер)
+const strelkaVnizOpenCloseLeftPanel = document.querySelector(".element-open-close-left-panel");
+const strelkaVverhOpenCloseLeftPanel = document.querySelector(".element2-open-close-left-panel");
+openCloseDetailsButtonLeft.forEach(function (left) {
+    // Делаем полноценные функции для левой панели
+    let isOpen = false; // комментарий выше относится к данной переменной.
+    left.addEventListener("click", function () {
+        openCloseDetailsSpheres.forEach(openCloseDetailsSpheres => {
+            if (isOpen) {
+                openCloseDetailsSpheres.removeAttribute("open");
+            } else {
+                openCloseDetailsSpheres.setAttribute("open", true)
+            }
+        });
+
+
+        isOpen = !isOpen;
+
+        if (isOpen) {
+            strelkaVnizOpenCloseLeftPanel.style.display = "none";
+            strelkaVverhOpenCloseLeftPanel.style.display = "block";
+        } else {
+            strelkaVverhOpenCloseLeftPanel.style.display = "none";
+            strelkaVnizOpenCloseLeftPanel.style.display = "block";
+        }
     });
 
-    sections.forEach((section) => {
-        observer.observe(section);
+    // openCloseDetailsButtonLeft.forEach(function (left) {
+    //     let isOpen = false;
+    //     left.addEventListener("click", function () {
+    //         openCloseDetailsSpheres.forEach(openCloseDetailsSpheres => {
+    //             if (isOpen) {
+    //                 openCloseDetailsSpheres.removeAttribute("open");
+    //             } else {
+    //                 openCloseDetailsSpheres.setAttribute("open", true)
+    //             }
+    //         });
+
+
+    //         isOpen = !isOpen;
+
+    //         if (isOpen) {
+    //             zakladkaOpenCloseLeftPanel.classList.add("opened-details-left-panel");
+    //             zakladkaOpenCloseLeftPanel.classList.remove("closed-details-left-panel");
+    //         } else {
+    //             zakladkaOpenCloseLeftPanel.classList.add("closed-details-left-panel");
+    //             zakladkaOpenCloseLeftPanel.classList.remove("opened-details-left-panel");
+    //         }
+    //     });
+
+    //     left.addEventListener("mouseenter", function () {
+    //         if (!isOpen) {
+    //             zakladkaOpenCloseLeftPanel.classList.add("closed-details-left-panel");
+    //             zakladkaOpenCloseLeftPanel.classList.remove("opened-details-left-panel");
+    //         }
+    //     });
+
+    // Добавляем функции для закрытия открытых элементов при нажатии на любую область блока и запрещаем, если кнопка открытия/закрытия активна.
+    const blockBottomLeftPanel = document.querySelector(".content-top-left-panel");
+    blockBottomLeftPanel.addEventListener("click", event => {
+        const target = event.target;
+        const isDetailsSpheres = target.closest(".details-spheres");
+
+        if (!isOpen && !isDetailsSpheres) {
+            // Если open-close-details неактивна и клик был на другом элементе, разрешаем удаление значения open
+            const detailsList = document.querySelectorAll(".details-spheres");
+            detailsList.forEach(details => {
+                details.removeAttribute("open");
+            });
+        }
     });
 
-    // Добавим событие прокрутки для обработки изменений на мобильных устройствах
-    let lastKnownScrollPosition = 0;
-    let ticking = false;
+    // Добавляем обработчик клика на элементы details с классом details-question
+    const detailsList = document.querySelectorAll(".details-spheres");
+    detailsList.forEach(details => {
+        details.addEventListener("click", event => {
+            // Проверяем, активно ли open-close-details
+            if (isOpen) {
+                event.preventDefault(); // Предотвращаем удаление значения open
+            }
+        });
+    });
 
-    function handleScroll() {
-        lastKnownScrollPosition = window.scrollY;
-
-        if (!ticking) {
-            window.requestAnimationFrame(() => {
-                handleIntersection(sections.map((section) => ({ target: section })), observer);
-                ticking = false;
-            });
-
-            ticking = true;
-        }
-    }
-
-    document.addEventListener('scroll', handleScroll);
+    const summaryList = document.querySelectorAll("img");
+    summaryList.forEach(img => {
+        img.addEventListener("click", event => {
+            event.stopPropagation();
+        });
+    });
 });
